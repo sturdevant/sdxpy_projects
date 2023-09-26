f279d110298fa1e0ec92353de15d5aaebf04b7a0
sturdevant <sturdevant2@wisc.edu>
2023-09-25 19:36:37 -0500

    update naive_hash.py

--- duplicates/naive_hash.py
+++ duplicates/naive_hash.py
@@ -1,8 +1,9 @@
 def naive_hash(data):
     return sum(data) % 13
 
-example = bytes("hashing", "utf-8")
-for i in range(1, len(example) + 1):
-    substring = example[:i]
-    hash = naive_hash(substring)
-    print(f"{hash:2} {substring}")
+if __name__ == "__main__":
+    example = bytes("hashing", "utf-8")
+    for i in range(1, len(example) + 1):
+        substring = example[:i]
+        hash = naive_hash(substring)
+        print(f"{hash:2} {substring}")
