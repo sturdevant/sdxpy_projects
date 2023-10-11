794648885baedaa58ea83950af20d8352c7de6f0
sturdevant <sturdevant2@wisc.edu>
2023-10-10 22:28:24 -0500

    add mock_time.py

---
+++ protocol/mock_time.py
@@ -0,0 +1,13 @@
+import time
+
+def elapsed(since):
+    return time.time() - since
+
+def mock_time():
+    return 200
+
+def test_elapsed():
+    time.time = mock_time
+    assert elapsed(50) == 150
+
+test_elapsed()
