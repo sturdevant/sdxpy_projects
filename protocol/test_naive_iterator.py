345fa8cdabce0ceccca7f438e87092af93c6f700
sturdevant <sturdevant2@wisc.edu>
2023-10-18 19:49:09 -0500

    add tests for empty string and nested loop

--- protocol/test_naive_iterator.py
+++ protocol/test_naive_iterator.py
@@ -1,3 +1,4 @@
+import pytest
 from naive_iterator import NaiveIterator
 
 def gather(buffer):
@@ -10,5 +11,20 @@ def test_naive_buffer():
     buffer = NaiveIterator(["ab", "c"])
     assert gather(buffer) == "abc"
 
+def test_naive_buffer_empty_string():
+    buffer = NaiveIterator(["a", ""])
+    with pytest.raises(IndexError):
+        assert gather(buffer) == "a"
+
+def test_naive_buffer_nested_loop():
+    buffer = NaiveIterator(["a", "b"])
+    result = ""
+    for outer in buffer:
+        for inner in buffer:
+            result += inner
+    assert result == "abab"
+
 if __name__ == "__main__":
     test_naive_buffer()
+    test_naive_buffer_empty_string()
+    test_naive_buffer_nested_loop()
