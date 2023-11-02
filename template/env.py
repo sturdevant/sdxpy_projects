197d7324565dcef2b25f9c2354d5dc74592d56b3
sturdevant <sturdevant2@wisc.edu>
2023-11-01 20:53:21 -0500

    add env.py for variable management

---
+++ template/env.py
@@ -0,0 +1,15 @@
+class Env:
+    def __init__(self, initial):
+        self.stack = [initial.copy()]
+
+    def push(self, frame):
+        self.stack.append(frame)
+
+    def pop(self):
+        self.stack.pop()
+
+    def find(self, name):
+        for frame in reversed(self.stack):
+            if name in frame:
+                return frame[name]
+        return None
