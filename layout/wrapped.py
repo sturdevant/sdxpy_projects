d8988180ea5dec2a0b1fef9861e5c53230f9e89d
sturdevant <sturdevant2@wisc.edu>
2023-11-14 22:42:34 -0600

    add wrapped.py

---
+++ layout/wrapped.py
@@ -0,0 +1,42 @@
+from placed import PlacedBlock, PlacedCol, PlacedRow
+
+class WrappedBlock(PlacedBlock):
+    def wrap(self):
+        return self
+
+class WrappedCol(PlacedCol):
+    def wrap(self):
+        return PlacedCol(*[c.wrap() for c in self.children()])
+
+class WrappedRow(PlacedRow):
+    def __init__(self, width, *children):
+        super().__init__(*children)
+        assert width >= 0, "Need non-negative width"
+        self.width = width
+
+    def get_width(self):
+        return self.width
+
+    def wrap(self):
+        children = [c.wrap() for c in self.children]
+        rows = self._bucket(children)
+        new_rows = [PlacedRow(*r) for r in rows]
+        new_col = PlacedCol(*new_rows)
+        return PlacedRow(new_col)
+
+    def _bucket(self, children):
+        result = []
+        current_row = []
+        current_x = 0
+
+        for child in children:
+            child_width = child.get_width()
+            if (current_x + child_width) <= self.width:
+                current_row.append(child)
+                current_x += child_width
+            else:
+                result.append(current_row)
+                current_row = [child]
+                current_x = child_width
+        result.append(current_row)
+        return result
