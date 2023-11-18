e47f60fca6e8a1763f2b729ec7125b1954bb5029
sturdevant <sturdevant2@wisc.edu>
2023-11-17 22:37:14 -0600

    update util to include all_eq

--- perf/util.py
+++ perf/util.py
@@ -2,3 +2,6 @@ def dict_match(d, prototype):
     if set(d.keys()) != set(prototype.keys()):
         return False
     return all(type(d[k]) == type(prototype[k]) for k in d)
+
+def all_eq(*values):
+    return (not values) or all(v == values[0] for v in values)
