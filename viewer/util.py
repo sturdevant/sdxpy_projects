ba91b31b094a61a25dd9001e28add9cbe42af607
sturdevant <sturdevant2@wisc.edu>
2024-01-12 15:12:57 -0600

    update util.py to generate lines of characters

--- viewer/util.py
+++ viewer/util.py
@@ -8,3 +8,12 @@ def open_log(filename):
 
 def log(*args):
     print(*args, file=LOG)
+
+from string import ascii_lowercase
+
+def make_lines(num_lines):
+    result = []
+    for i in range(num_lines):
+        ch = ascii_lowercase[i % len(ascii_lowercase)]
+        result.append(ch + "".join(str(j % 10) for j in range(i)))
+    return result
