737365f7bce89e9adc4236f691d7b59153340817
sturdevant <sturdevant2@wisc.edu>
2024-01-14 22:19:29 -0600

    update util.py to include start function

--- viewer/util.py
+++ viewer/util.py
@@ -17,3 +17,15 @@ def make_lines(num_lines):
         ch = ascii_lowercase[i % len(ascii_lowercase)]
         result.append(ch + "".join(str(j % 10) for j in range(i)))
     return result
+
+ROW = 0
+COL = 1
+
+def start():
+    num_lines, logfile = int(sys.argv[1]), sys.argv[2]
+    size = None
+    if len(sys.argv) > 3:
+        size = (int(sys.argv[3]), int(sys.argv[4]))
+    lines = make_lines(num_lines)
+    open_log(logfile)
+    return size, lines
