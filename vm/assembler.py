6c98a436f29afed1f62872ec1fcb48ee625971fe
sturdevant <sturdevant2@wisc.edu>
2024-02-09 22:15:59 -0600

    implement finding labels in assembler.py

--- vm/assembler.py
+++ vm/assembler.py
@@ -13,3 +13,18 @@ class Assembler:
         ]
         program = self._to_text(compiled)
         return program
+    
+    def _find_labels(self, lines):
+        result = {}
+        loc = 0
+        for ln in lines:
+            if self._is_label(ln):
+                label = ln[:-1].strip()
+                assert label not in result, f"Duplicated {label}"
+                result[label] = loc
+            else:
+                loc += 1
+        return result
+
+    def _is_label(self, line):
+        return line.endswith(":")
