57266f3625dc0f19ecb852e8951a3ea2292509a5
sturdevant <sturdevant2@wisc.edu>
2024-03-03 19:14:58 -0600

    finish implementing vm_step.py

--- debugger/vm_step.py
+++ debugger/vm_step.py
@@ -26,3 +26,9 @@ class VirtualMachineStep(VirtualMachineBase):
         op, arg0, arg1 = self.decode(instruction)
         assert op in OPS_LOOKUP, f"Unknown op code {op} at {addr}"
         return f"{OPS_LOOKUP[op]} | {arg0} | {arg1}"
+
+    def read(self, prompt):
+        return self.reader(prompt).strip()
+
+if __name__ == "__main__":
+    VirtualMachineStep.main()
