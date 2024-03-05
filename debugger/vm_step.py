dc97c050cbfd5eb869fb822fb94e76b6808d8ac3
sturdevant <sturdevant2@wisc.edu>
2024-03-04 21:11:38 -0600

    implement interact in vm_step.py

--- debugger/vm_step.py
+++ debugger/vm_step.py
@@ -22,6 +22,29 @@ class VirtualMachineStep(VirtualMachineBase):
         op, arg0, arg1 = self.decode(instruction)
         self.execute(op, arg0, arg1)
 
+    def interact(self, addr):
+        while self.state == VMState.STEPPING:
+            try:
+                command = self.read(f"{addr:06x} [dmqrs]> ")
+                if not command:
+                    continue
+                elif command in {"d", "dis"}:
+                    self.write(self.disassemble(addr, self.ram[addr]))
+                elif command in {"m", "memory"}:
+                    self.show()
+                elif command in {"q", "quit"}:
+                    self.state = VMState.FINISHED
+                    break
+                elif command in {"r", "run"}:
+                    self.state = VMState.RUNNING
+                    break
+                elif command in {"s", "step"}:
+                    break
+                else:
+                    self.write(f"Unknown command '{command}'")
+            except EOFError:
+                self.state = VMState.FINISHED
+
     def disassemble(self, addr, instruction):
         op, arg0, arg1 = self.decode(instruction)
         assert op in OPS_LOOKUP, f"Unknown op code {op} at {addr}"
