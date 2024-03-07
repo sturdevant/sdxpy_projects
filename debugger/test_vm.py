468b8b2aad0e6600ffe42322fc88f39d11647667
sturdevant <sturdevant2@wisc.edu>
2024-03-06 22:12:29 -0600

    implement reader in test_vm.py

--- debugger/test_vm.py
+++ debugger/test_vm.py
@@ -1,6 +1,16 @@
 from assembler import Assembler
 from vm_step import VirtualMachineStep as VM
 
+class Reader:
+    def __init__(self, *args):
+        self.commands = args
+        self.index = 0
+
+    def __call__(self, prompt):
+        assert self.index < len(self.commands)
+        self.index += 1
+        return self.commands[self.index - 1]
+
 class Writer:
     def __init__(self):
         self.seen = []
